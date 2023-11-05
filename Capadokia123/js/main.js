fe96a349ded609c63b06b896ffc6eaae9a80ca41
yarikleva <levchenko153729@gmail.com>
2023-11-05 17:36:04 +0300

    Cappadocia +++

--- Capadokia123/js/main.js
+++ Capadokia123/js/main.js
@@ -1,7 +1,7 @@
-jQuery(function() {
-    jQuery('.fairy-tail__slider, .our-trip__slider').slick({
-            prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="../images/стрелка back fairy-tail .svg" alt=""></button>',
-            nextArrow: '<button type="button" class="slick-btn slick-next"><img src="../images/стрелка next fairy-tail .svg" alt=""></button>',
+$(function() {
+    $('.fairy-tail__slider, .our-trip__slider').slick({
+            prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-right.svg" alt=""></button>',
+            nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-left.svg" alt=""></button>',
             autoplay: true,
             fade: true,
             responsive: [{
